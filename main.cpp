dfb9c6379aaaecf0425086bcda3f0fbbfdf7b056
suang <146166276+suanggggg@users.noreply.github.com>
2024-07-10 01:38:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,28 +8,59 @@ using namespace std;
 
 class TestQueueAllocator
 {
-	typedef ObjectPool<TestQueueAllocator, StackAllocator<TestQueueAllocator>> ObjectPool;
+	typedef ObjectPool<TestQueueAllocator, QueueAllocator<TestQueueAllocator>> ObjectPool;
 
 public:
 	TestQueueAllocator()
 	{
-		cout << "TestQueueAllocator()" << endl;
+		cout << "TestQueueAllocator()" << endl << endl;
 	}
 
 	~TestQueueAllocator()
 	{
-		cout << "~TestQueueAllocator()" << endl;
+		cout << "~TestQueueAllocator()" << endl << endl;
 	}
 
 	void* operator new(size_t size)
 	{
-		cout << "new TestQueueAllocator" << endl;
+		cout << "new TestQueueAllocator" << endl << endl;
 		return pool.allocate(size);
 	}
 
 	void operator delete(void* p)
 	{
-		cout << "delete TestQueueAllocator" << endl;
+		cout << "delete TestQueueAllocator" << endl << endl;
+		pool.deallocate(p);
+	}
+
+private:
+	static ObjectPool pool;
+};
+
+class TestStackAllocator
+{
+	typedef ObjectPool<TestStackAllocator, StackAllocator<TestStackAllocator>> ObjectPool;
+
+public:
+	TestStackAllocator()
+	{
+		cout << "TestStackAllocator()" << endl << endl;
+	}
+
+	~TestStackAllocator()
+	{
+		cout << "~TestStackAllocator()" << endl << endl;
+	}
+
+	void* operator new(size_t size)
+	{
+		cout << "new TestStackAllocator" << endl << endl;
+		return pool.allocate(size);
+	}
+
+	void operator delete(void* p)
+	{
+		cout << "delete TestStackAllocator" << endl << endl;
 		pool.deallocate(p);
 	}
 
@@ -38,10 +69,13 @@ private:
 };
 
 TestQueueAllocator::ObjectPool TestQueueAllocator::pool;
+TestStackAllocator::ObjectPool TestStackAllocator::pool;
 
 int main()
 {
 	TestQueueAllocator* a = new TestQueueAllocator();
 	delete a;
+	TestStackAllocator* b = new TestStackAllocator();
+	delete b;
 	return 0;
 }
